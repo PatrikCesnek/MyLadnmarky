4f4e59c839bb76c6350e34d46a6b7cdd0ab2600e
Patrik Cesnek <pcesnek290@gmail.com>
2025-02-03 17:28:34 +0100

    Initial Commit

---
+++ LandmarkyTests/LandmarkyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkyTests.swift
+//  LandmarkyTests
+//
+//  Created by Patrik Cesnek on 03/02/2025.
+//
+
+import Testing
+@testable import Landmarky
+
+struct LandmarkyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
